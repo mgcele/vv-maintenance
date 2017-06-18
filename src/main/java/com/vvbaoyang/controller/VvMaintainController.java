2e6ff84d002b8d94c2f4e84614290bce38736aa6
mgcele <hong_mgcele@163.com>
2017-06-19 01:58:26 +0800

    官网接口

--- src/main/java/com/vvbaoyang/controller/VvMaintainController.java
+++ src/main/java/com/vvbaoyang/controller/VvMaintainController.java
@@ -2,6 +2,7 @@ package com.vvbaoyang.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author mgcele
@@ -29,4 +30,46 @@ public class VvMaintainController {
         return "order1";
     }
 
+    @RequestMapping(value = "/service.html", method = RequestMethod.GET)
+    public String serverce(){
+
+        return "service";
+    }
+
+    @RequestMapping(value = "/index.html", method = RequestMethod.GET)
+    public String index1(){
+
+        return "index";
+    }
+
+    @RequestMapping(value = "/advantage.html", method = RequestMethod.GET)
+    public String advantage(){
+
+        return "advantage";
+    }
+
+    @RequestMapping(value = "/contact.html", method = RequestMethod.GET)
+    public String contact(){
+
+        return "contact";
+    }
+
+    @RequestMapping(value = "/control.html", method = RequestMethod.GET)
+    public String control(){
+
+        return "control";
+    }
+
+
+    @RequestMapping(value = "/price.html", method = RequestMethod.GET)
+    public String price(){
+
+        return "price";
+    }
+
+    @RequestMapping(value = "/qualifications.html", method = RequestMethod.GET)
+    public String qualifications(){
+
+        return "qualifications";
+    }
 }
